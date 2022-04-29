dd3cab7f6d13666076e1490db9a5b10a77d232d5
Egor <goldenyakegor@gmail.com>
2022-04-29 23:45:54 +0300

    delete store because conflict with App

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
 import React from 'react';
-import {Provider} from 'react-redux';
 import {Route, Routes} from 'react-router-dom';
 import './App.css';
 import {Login} from './components/Login/Login';
 import {Profile} from './components/Profile/Profile';
 import {Register} from './components/Register/Register';
-import {store} from './redux/store';
 
 function App() {
     return (
         <div className='App'>
-            <Provider store={store}>
-                Hello, World)
-                <Routes>
-                    <Route path="/login" element={<Login/>}/>
-                    <Route path='/register' element={<Register/>}/>
-                    <Route path='/profile' element={<Profile/>}/>
-                </Routes>
-            </Provider>
+            Стартовая страница
+            <Routes>
+                <Route path="/login" element={<Login/>}/>
+                <Route path='/register' element={<Register/>}/>
+                <Route path='/profile' element={<Profile/>}/>
+            </Routes>
         </div>
     );
 }

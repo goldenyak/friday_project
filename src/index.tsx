f1e9c70e4d1bc2e1644b92bb4814c15878835b02
Egor <goldenyakegor@gmail.com>
2022-05-01 20:39:24 +0300

    refactoring code

--- src/index.tsx
+++ src/index.tsx
@@ -9,10 +9,11 @@ const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
+
     <React.StrictMode>
-        <BrowserRouter>
-            <App/>
-        </BrowserRouter>
+            <BrowserRouter>
+                <App/>
+            </BrowserRouter>
     </React.StrictMode>
 );
 

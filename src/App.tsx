a136914f32022e2d5ed5981370e220dfe21e0a16
Egor <goldenyakegor@gmail.com>
2022-05-01 22:04:44 +0300

    made login form

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import {Route, Routes} from 'react-router-dom';
-import './App.css';
 import {Login} from './components/Login/Login';
 import {Profile} from './components/Profile/Profile';
 import {Register} from './components/Register/Register';
 
 function App() {
     return (
-        <div className='App'>
-                Hello, World)
+        <div>
                 <Routes>
                     <Route path="/login" element={<Login/>}/>
                     <Route path='/register' element={<Register/>}/>
--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import l from "./Login.module.scss";
 
 export const Login = () => {
     return (
-        <div>
-            Страница LOGIN
+        <div className={l.loginBox}>
+            <h1>It-incubator</h1>
+            <h2>Sign In</h2>
+            <form>
+                <div className={l.userBox}>
+                    <input type="text" name="" required={true}/>
+                    <label>Username</label>
+                </div>
+                <div className={l.userBox}>
+                    <input type="password" name="" required={true}/>
+                    <label>Password</label>
+                </div>
+                <button>
+                    Login
+                </button>
+                <h3>Don’t have an account?</h3>
+                <h4>Sign Up</h4>
+            </form>
         </div>
-    );
+    )
+
 };

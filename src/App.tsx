c2b1de63e9799cb23a3c5229327524c184583836
onethps <onethps@gmail.com>
2022-05-04 02:25:56 +0300

    -deleted .idea

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ function App() {
                 <Route path="/login" element={<Login/>}/>
                 <Route path='/register' element={<Register/>}/>
                 <Route path='/profile' element={<Profile/>}/>
+
                 <Route path={'*'}  element={ <Register/> }/>
             </Routes>
         </div>
